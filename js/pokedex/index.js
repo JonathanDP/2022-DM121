33af1b8a0c527806bb6ccccde862d366e32a30b7
JonathanDP <jonathan.donizetti@pg.inatel.br>
2022-09-24 17:20:59 -0300

    adding all pokemons and formatting pokedex screen

--- js/pokedex/index.js
+++ js/pokedex/index.js
@@ -1,23 +1,54 @@
+let elementsColor = {
+    bug: "#A7B723",
+    dark: "#75574C",
+    dragon: "#7037FF",
+    electric: "#F9CF30",
+    fairy: "#E69EAC",
+    fighting: "#C12239",
+    fire: "#F57D31",
+    flying: "#A891EC",
+    ghost: "#70559B",
+    grass: "#74CB48",
+    ground: "#DEC16B",
+    ice: "#9AD6DF",
+    normal: "#AAA67F",
+    poison: "#A43E9E",
+    psychic: "#FB5584",
+    rock: "#B69E31",
+    steel: "#B7B9D0",
+    water: "#6493EB",
+}
+
 async function fetchData(pokeNumber) {
     const response = await fetch(
-      "https://pokeapi.co/api/v2/pokemon/" + pokeNumber
+        "https://pokeapi.co/api/v2/pokemon/" + pokeNumber
     );
     const pokemon = await response.json();
     return pokemon;
-  }
-  
-  async function createPokemon(pokeNumber) {
+}
+
+async function createPokemon(pokeNumber) {
     const pokemon = await fetchData(pokeNumber);
+    let element = `${pokemon.types[0].type['name']}`;
     const div = document.createElement("div");
-    const h2 = document.createElement("h2");
+    const nameDiv = document.createElement("div");
+    const name = document.createElement("h2");
+    const elementName = document.createElement("h2");
+    const order = document.createElement("h4")
     const img = document.createElement("img");
-    h2.textContent = `#${pokemon.order} - ${pokemon.name}`;
+    name.textContent = `${pokemon.name}`;
+    order.textContent = `#${pokemon.order}`
+    elementName.textContent = element;
     img.src = pokemon.sprites.front_default;
-    div.appendChild(h2);
+    div.appendChild(order);
     div.appendChild(img);
+    div.appendChild(elementName);
+    nameDiv.appendChild(name);
+    div.appendChild(nameDiv);
+    div.style.backgroundColor = elementsColor[element]
     document.body.appendChild(div);
-  }
-  
-  const pokeNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  pokeNumbers.forEach(createPokemon);
-  
+}
+
+for (var nPoke = 0; nPoke <= 152; nPoke++) {
+    createPokemon(nPoke)
+ }
